6e066aa381b80c8b82bdb76c206ae5fa5e242694
unknown <jangrak019@gmail.com>
2024-12-01 10:22:41 +0530

    changes in abc

--- middleware.js
+++ middleware.js
@@ -8,24 +8,20 @@ export function middleware(request) {
   const port = url.port || '80';  // Default to port 80 if port is not available
   console.log("url????", url, port, host);
 
-  // https://multi-domain-app-next.vercel.app/
-  // https://multi-domain-app-next-kapils-projects-9c8b1734.vercel.app/
+  // Check if the current URL is already the target URL
+  const currentPath = request.nextUrl.pathname;
+
   // Define domain-specific routing logic
-
-
-  if (host === 'multi-domain-app-next.vercel.app' ) {
+  if (host === 'multi-domain-app-next.vercel.app' && currentPath !== '/university') {
     console.log("Redirecting to /university");
     return NextResponse.redirect(new URL('/university', request.nextUrl));
-    
-  }else if (host === 'multi-domain-app-next-kapils-projects-9c8b1734.vercel.app') {
+  } else if (host === 'multi-domain-app-next-kapils-projects-9c8b1734.vercel.app' && currentPath !== '/colleges') {
+    console.log("Redirecting to /colleges");
+    return NextResponse.redirect(new URL('/colleges', request.nextUrl));
+  } else if (host === 'multi-domain-app-next-kwkfxmvw8-kapils-projects-9c8b1734.vercel.app' && currentPath !== '/colleges') {
     console.log("Redirecting to /colleges");
     return NextResponse.redirect(new URL('/colleges', request.nextUrl));
   }
-  else if (host === 'multi-domain-app-next-kwkfxmvw8-kapils-projects-9c8b1734.vercel.app') {
-    console.log("Redirecting to /colleges");
-    return NextResponse.redirect(new URL('/colleges', request.nextUrl));
-  }
-  
 
   console.log('No redirection, allowing request to proceed.');
   return NextResponse.next();
