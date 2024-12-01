b444c35c30366c504bc82886be39d78c7620e222
unknown <Kapil@example.com>
2024-12-01 10:08:40 +0530

    changes

--- middleware.js
+++ middleware.js
@@ -8,16 +8,16 @@ export function middleware(request) {
   const port = url.port || '80';  // Default to port 80 if port is not available
   console.log("url????", url, port, host);
 
+  // https://multi-domain-app-next.vercel.app/
+  // https://multi-domain-app-next-kapils-projects-9c8b1734.vercel.app/
   // Define domain-specific routing logic
-  // if (port === '3000' ) {
-  //   console.log("Redirecting to /university");
-  //   return NextResponse.redirect(new URL('/university', request.nextUrl));
-  // }
-
-  // if (port === '3001') {
-  //   console.log("Redirecting to /colleges");
-  //   return NextResponse.redirect(new URL('/colleges', request.nextUrl));
-  // }
+  if (port === 'https://multi-domain-app-next.vercel.app/' ) {
+    console.log("Redirecting to /university");
+    return NextResponse.redirect(new URL('/university', request.nextUrl));
+  }else if (port === 'https://multi-domain-app-next-kapils-projects-9c8b1734.vercel.app/') {
+    console.log("Redirecting to /colleges");
+    return NextResponse.redirect(new URL('/colleges', request.nextUrl));
+  }
 
   console.log('No redirection, allowing request to proceed.');
   return NextResponse.next();
