776559cf8c0d87258d220a669813dd854da5f10b
unknown <jangrak019@gmail.com>
2024-12-01 10:33:41 +0530

    changes in abc drewrite

--- middleware.js
+++ middleware.js
@@ -3,26 +3,22 @@ import { NextResponse } from 'next/server';
 export function middleware(request) {
   console.log('Middleware executed for:', request.nextUrl.pathname);
 
-  const url = request.nextUrl.clone();
   const host = request.headers.get('host'); // Get the domain name
-  const port = url.port || '80';  // Default to port 80 if port is not available
-  console.log("url????", url, port, host);
-
   const currentPath = request.nextUrl.pathname;
 
-  // Define domain-specific routing logic
-  if (host === 'multi-domain-app-next.vercel.app' && currentPath !== '/university') {
-    console.log("Redirecting to /university");
-    return NextResponse.redirect(new URL('/university', request.nextUrl));
-  } else if (host === 'multi-domain-app-next-kapils-projects-9c8b1734.vercel.app' && currentPath !== '/colleges') {
-    console.log("Redirecting to /colleges");
-    return NextResponse.redirect(new URL('/colleges', request.nextUrl));
-  } else if (host === 'multi-domain-app-next-kwkfxmvw8-kapils-projects-9c8b1734.vercel.app' && currentPath !== '/colleges') {
-    console.log("Redirecting to /colleges");
-    return NextResponse.redirect(new URL('/colleges', request.nextUrl));
+  // Domain-specific routing logic for re-writing URLs
+  if (host === 'multi-domain-app-next.vercel.app' && currentPath === '/') {
+    console.log("Rewriting to /university");
+    return NextResponse.rewrite(new URL('/university', request.nextUrl));
+  } else if (host === 'multi-domain-app-next-kapils-projects-9c8b1734.vercel.app' && currentPath === '/') {
+    console.log("Rewriting to /colleges");
+    return NextResponse.rewrite(new URL('/colleges', request.nextUrl));
+  } else if (host === 'multi-domain-app-next-kwkfxmvw8-kapils-projects-9c8b1734.vercel.app' && currentPath === '/') {
+    console.log("Rewriting to /colleges");
+    return NextResponse.rewrite(new URL('/colleges', request.nextUrl));
   }
 
-  console.log('No redirection, allowing request to proceed.');
+  console.log('No rewrite, allowing request to proceed.');
   return NextResponse.next();
 }
 
